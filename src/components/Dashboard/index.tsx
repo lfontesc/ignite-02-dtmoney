507dc0f26f366243cffc68df02b6dfcd62102771
Lucas Fontes <lfonteesc@gmail.com>
2021-09-21 17:30:53 -0300

    add repository

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Summary } from "../Summary";
+import { Container } from "./styles";
+
+export function Dashboard() {
+  return (
+    <div>
+      <Container>
+        <Summary />
+      </Container>
+    </div>
+  );
+}
